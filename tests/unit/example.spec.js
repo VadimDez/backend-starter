6adaf7a867a81f6a141388cb1c723fa5f9d0d0ad
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-08-10 22:56:21 +0200

    tests

---
+++ tests/unit/example.spec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Vadym Yatsyuk on 10.08.18
+ */
+const expect = require('chai').expect;
+
+describe('sample test', () => {
+	it('should pass', () => {
+		expect(true).to.equal(true);
+	});
+});
