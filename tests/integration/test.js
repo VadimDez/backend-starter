297342bcf65545870990ecd847bef36f5dc51b7f
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-08-25 23:22:06 +0200

    add test

--- tests/integration/test.js
+++ tests/integration/test.js
@@ -6,12 +6,19 @@ const { expect } = require('chai');
 const app = require('../../index');
 
 describe('sample test', () => {
-	it('should pass', async () => {
-		request(app)
+	it('should pass', () => {
+		return request(app)
 			.get('/v1/test')
 			.expect(200)
 			.then((res) => {
 				expect(res.body).to.deep.equal({ status: 'ok' });
 			});
+	});
+
+
+	it('should return 200 when accessing protected route with no auth header (only for test env)', () => {
+		return request(app)
+			.get('/v1/protected')
+			.expect(200);
 	});
 });
