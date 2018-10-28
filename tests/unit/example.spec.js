6b911877f9eb4b5f65fc2ad85fd14e5c4151e253
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2018-10-28 07:57:35 +0100

    remove mocha and chai

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,10 +1,9 @@
 /**
  * Created by Vadym Yatsyuk on 10.08.18
  */
-const expect = require('chai').expect;
 
 describe('sample test', () => {
 	it('should pass', () => {
-		expect(true).to.equal(true);
+		expect(true).toBe(true);
 	});
 });
