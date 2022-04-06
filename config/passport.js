8a3553291549987c14b0a96ad3177a48afed92ba
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2022-04-06 10:09:04 +0200

    update dependencies

--- config/passport.js
+++ config/passport.js
@@ -2,10 +2,10 @@
  * Created by Vadym Yatsyuk on 21.08.18
  */
 const APIStrategy = require("ibmcloud-appid").APIStrategy;
-const vars = require('./vars');
+const vars = require("./vars");
 
 exports.APP_ID_STRATEGY = new APIStrategy({
-	oauthServerUrl: vars.APP_ID.OAUTH_SERVER_URL,
-	tenantId: vars.APP_ID.TENTANT_ID,
-	clientId: vars.APP_ID.CLIENT_ID
+  oauthServerUrl: vars.APP_ID.OAUTH_SERVER_URL,
+  tenantId: vars.APP_ID.TENTANT_ID,
+  clientId: vars.APP_ID.CLIENT_ID,
 });
